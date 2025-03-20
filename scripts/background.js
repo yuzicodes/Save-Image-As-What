fa4f06d3b1e64d115d4b28bd31f1589b65438048
Yuzi <yuzicodes@gmail.com>
2025-03-21 03:01:17 +0530

    1.0

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "saveImageAs",
-    title: "Save Image As What?",
+    title: "Save Image As What",
     contexts: ["image"]
   });
   chrome.contextMenus.create({ id: "saveAsPNG", parentId: "saveImageAs", title: "PNG", contexts: ["image"] });
